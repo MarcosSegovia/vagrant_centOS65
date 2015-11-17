84ec17e72d56ffdcb39b2db558dcac3b24c7abf2
Marcos <velozmarkdrea@gmail.com>
2015-11-17 15:03:30 +0100

    adding new packages in modules.sh and adding gitignore shared/www

--- vagrant/scripts/install_modules.sh
+++ vagrant/scripts/install_modules.sh
@@ -1,13 +1,17 @@
 
 dir='/vagrant/modules/'
 
-puppet module install puppetlabs/stdlib --modulepath $dir --version 4.9.0;
-puppet module install puppetlabs/concat --modulepath $dir --version 1.2.4;
-puppet module install example42/puppi --modulepath $dir --version 2.1.12;
 puppet module install example42/yum --modulepath $dir --version 2.1.25;
 puppet module install example42/php  --modulepath $dir --version 2.0.25;
 puppet module install puppetlabs-apache --modulepath $dir --version 1.6.0;
 puppet module install puppetlabs/mysql --modulepath $dir --version 3.6.1;
 puppet module install example42/memcached --modulepath $dir --version 3.0.5;
+puppet module install crayfishx/firewalld --modulepath $dir --version 1.2.3;
+puppet module install example42-timezone --modulepath $dir --version 2.0.13;
+puppet module install example42/ntp --modulepath $dir --version 2.0.15;
+
+yum install -y git
 git clone https://github.com/MarcosSegovia/my_module.git;
 mv my_module $dir;
+
+exit 0;
