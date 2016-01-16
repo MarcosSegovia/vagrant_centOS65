f1081ebacb4cd916d4a4910c2fe9fcdb679152cb
Marcos Segovia <velozmarkdrea@gmail.com>
2016-01-16 15:22:47 +0100

    setting create to true in synced_folder and installing composer in VM

--- vagrant/scripts/install_modules.sh
+++ vagrant/scripts/install_modules.sh
@@ -10,6 +10,7 @@ puppet module install example42/memcached --modulepath $dir --version 3.0.5;
 puppet module install crayfishx/firewalld --modulepath $dir --version 1.2.3;
 puppet module install example42-timezone --modulepath $dir --version 2.0.13;
 puppet module install example42/ntp --modulepath $dir --version 2.0.15;
+puppet module install willdurand-composer --modulepath $dir --version 1.1.1;
 
 if [ ! -d "$dir_my_module" ]; then
   # Control will enter here if $DIRECTORY exists.
